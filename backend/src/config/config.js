ac33d52d9e5d75f2b9949f706071ab5a4e990493
Goutamsahu23 <goutamsahu602@gmail.com>
2025-11-27 09:04:02 +0530

    origin updated

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -9,6 +9,6 @@ module.exports = {
   port: process.env.PORT || 4000,
   mongoUri: process.env.MONGO_URI || 'mongodb+srv://goutamsahu602:heFEljZh96ehNuOs@cluster0.yf7o4r2.mongodb.net/eventdb',
   jwtSecret: process.env.JWT_SECRET || 'dev_secret',
-  frontendOrigin: process.env.FRONTEND_ORIGIN || 'http://localhost:3000',
+  frontendOrigin: process.env.FRONTEND_ORIGIN || 'https://event-management-p34s.vercel.app',
   nodeEnv: process.env.NODE_ENV || 'development',
 };
