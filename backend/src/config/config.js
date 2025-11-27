a5929726d4ccc5eab518060f790ce2279cd37338
Goutamsahu23 <goutamsahu602@gmail.com>
2025-11-27 08:36:26 +0530

    monogo string updated

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,7 +7,7 @@ dotenv.config({
 
 module.exports = {
   port: process.env.PORT || 4000,
-  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/eventdb',
+  mongoUri: process.env.MONGO_URI || 'mongodb+srv://goutamsahu602:heFEljZh96ehNuOs@cluster0.yf7o4r2.mongodb.net/eventdb',
   jwtSecret: process.env.JWT_SECRET || 'dev_secret',
   frontendOrigin: process.env.FRONTEND_ORIGIN || 'http://localhost:3000',
   nodeEnv: process.env.NODE_ENV || 'development',
